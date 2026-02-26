5610fea1c8bb38b139fe3124c9410302931a0c5b
sarahatawi-ctrl <srh.alatwi@gmail.com>
2026-02-26 12:47:51 +0300

    Add ChatbotView widget with WebView integration

---
+++ lib/ChatbotView.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class ChatbotView extends StatefulWidget {
+  const ChatbotView({super.key});
+
+  @override
+  State<ChatbotView> createState() => _ChatbotViewState();
+}
+
+class _ChatbotViewState extends State<ChatbotView> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  final String landbotUrl = "https://landbot.online/v3/H-3343792-TKASQ25BLXB8PUKP/index.html";
+
+  @override
+  void initState( ) {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) => setState(() => _isLoading = false),
+          onWebResourceError: (error) => setState(() => _isLoading = false),
+        ),
+      )
+      ..loadRequest(Uri.parse(landbotUrl));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('المساعد الذكي'),
+        backgroundColor: Colors.transparent, 
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF3A4F6C), Color(0xFF2C3E50)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: WebViewWidget(controller: _controller),
+                ),
+                if (_isLoading)
+                  const Center(
+                    child: CircularProgressIndicator(color: Colors.white),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
